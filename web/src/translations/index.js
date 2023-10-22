b133b30d71a038fcaaae2c2303ee1b64df1b3b58
Zpiks <24653659+Zpiks@users.noreply.github.com>
2023-10-22 14:59:03 +0200

    Update index.js
    Edit the index file to link the french translation file

--- web/src/translations/index.js
+++ web/src/translations/index.js
@@ -1,7 +1,8 @@
 import { i18nConfig }  from 'es2015-i18n-tag';
 import pl from './pl.json';
+import fr from './fr.json';
 
-const translations = { pl, en: {} };
+const translations = { pl, fr, en: {} };
 
 export default function setTranslation(lang) {
   console.log(translations)
